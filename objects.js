569ae592bfb8b73fc47dd377d068ce1d20af4f8e
Tania Whiting <tanwhiting@aol.com>
2018-08-16 03:26:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist  = { asia: "heat of the moment"};
 
-function updatePlayList = (playlist, name, songTitle){
+function updatePlayList (playlist, name, songTitle){
   playlist['name'] = 'songTitle';
   return playlist;
 }
