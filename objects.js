ce47166994f9f0300fc18062192097cf43a72a9c
Tania Whiting <tanwhiting@aol.com>
2018-08-16 03:46:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist (playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName){
-  delete playlist.Asia;
+  delete playlist.[artistName];
   return playlist;
 }
